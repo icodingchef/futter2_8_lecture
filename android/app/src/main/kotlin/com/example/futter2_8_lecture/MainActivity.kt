d3357abb3788e2b2b3eca576b23fdcc5107ba570
icodingchef <icodingchef@gmail.com>
2022-01-07 13:54:06 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/futter2_8_lecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.futter2_8_lecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+import 'chat_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Authentication(),
+    );
+  }
+}
+
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            headerBuilder: (context, constraints, double) {
+              return Padding(
+                padding: EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image(
+                    image: AssetImage('image/chef.gif'),
+                  ),
+                ),
+              );
+            },
+            providerConfigs: [EmailProviderConfiguration()],
+          );
+        }
+        return ChatScreen();
+      },
+    );
+  }
+}
